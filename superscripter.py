5831946ddb5857fc07c46ca308b5d6bdd97eae60
Chuanbo Pan <panchuanbo@DN800c4f07.stanford.edu>
2017-11-29 00:04:11 -0800

    sanitized superscripter

--- superscripter.py
+++ superscripter.py
@@ -1,7 +1,10 @@
+import latex
+
 def scripter(x):
     value = ''
     for word in x.split():
         for idx, c in enumerate(word):
+            c = latex.sanitize(c)
             value += str(c) if idx % 2 == 0 else ('^{(' + str(c) + ')}')
         value += '\\text{ }'
     return value
