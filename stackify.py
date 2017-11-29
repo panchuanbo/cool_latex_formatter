72ecd44b7138d1e1a3318fa5a43047355e754a8f
Chuanbo Pan <panchuanbo@DN800c4f07.stanford.edu>
2017-11-29 00:05:44 -0800

    changed stackify

--- stackify.py
+++ stackify.py
@@ -3,7 +3,7 @@ import latex
 def stack(current):
 	if current == "":
 		return ""
-	return "\\stackrel{\\text{" + latex.sanitize(current[0]) + "}}{" + stack(current[1:]) + "}"
+	return "\\stackrel{" + latex.sanitize(current[0]) + "}{" + stack(current[1:]) + "}"
 
 thing = raw_input("Enter your string to stackify: ")
 print("$$" + stack(thing) + "$$")
