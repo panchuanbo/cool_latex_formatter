423008b00294caed7dbe089ea61b76077aa74382
Chuanbo Pan <panchuanbo@DN800c4f07.stanford.edu>
2017-12-24 22:56:51 -0500

    some more cool formats

--- utils/supersub.py
+++ utils/supersub.py
@@ -9,12 +9,14 @@ def scripter(x, script='^'):
         value += '\\text{ }'
     return value
 
-def nester(x, script='^'):
+def nester(x, script='^', second_script=None):
+    start_len = len(x)
     def wahoo(x, s):
         if len(x) == 1:
             return s + latex.sanitize(x)
         else:
-            s = s + latex.sanitize(x[0]) + script + '{'
+            scr = script if second_script == None or start_len / len(x) < 2 else second_script
+            s = s + latex.sanitize(x[0]) + scr + '{'
             s = wahoo(x[1:], s) + '}'
             return s
 
---
+++ wahoooohaw.py
@@ -0,0 +1,4 @@
+import utils.supersub as ss
+
+x = raw_input("enter fun stuff: ")
+print('$$' + ss.nester(x, '^', '_') + '$$')
